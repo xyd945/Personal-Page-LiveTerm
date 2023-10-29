5a8a7877ddadaad99789007026d58639c4cda557
xyd945 <yudi@thingsprotocol.com>
2023-10-29 15:51:24 +0100

    replace profile logo

--- src/utils/bin/profile.ts
+++ src/utils/bin/profile.ts
@@ -11,22 +11,23 @@ const profile = async (args: string[]): Promise<string> => {
 ██║  ██║███████╗███████╗███████╗╚██████╔╝    ╚███╔███╔╝╚██████╔╝██║  ██║███████╗██████╔╝██╗
 ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝      ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝ ╚═╝
 
-             @@@@@@@@@@@@@                   my profile summary
-        @@@@               @@@@             -----------
-      @@                       @@            ABOUT
-    @@                           @@          ${config.name}
-  @@                               @@       ﰩ ${config.ps1_hostname}
- @@                         @@@     @@       
-@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-@@                                   @@     -----------
-@@                                   @@      CONTACT 
- @@                                 @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@                               @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-     @@@                        @@@         -----------
-        @@@                  @@@ @@          DONATE 
-         @|  @@@@@@@@@@@@@@@@   @@           - Hedera Wallet: 0.0.3305283-hhmsi
-         @|                      @@          - Ethereum Wallet: 0x224F597aabAcB821e96F0dd0E703175ebC9CfcDC
+    XXXX                        XXXX                  my profile summary
+      XXXX                    XXXX                   -----------
+        XXXX                XXXX                      ABOUT
+          XXXX            XXXX                        ${config.name}
+            XXXX        XXXX                         ﰩ ${config.ps1_hostname}
+              XXXX    XXXX                            
+                XXXXXXXX                             爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+              XXXX    XXXX                           -----------
+            XXXX        XXXX                          CONTACT 
+          XXXX            XXXX                        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+        XXXX                XXXX                      <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+      XXXX                    XXXX                    <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+    XXXX                        XXXX                 -----------
+                                                      DONATE 
+                                                      - Hedera Wallet: 0.0.3305283-hhmsi
+                                                      - Ethereum Wallet: 0x224F597aabAcB821e96F0dd0E703175ebC9C
+
 
 `;
   } else {
